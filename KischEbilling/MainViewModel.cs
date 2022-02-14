a077d596baeccf7943cb9b929c8edafaa76a69ce
Divar <divarr@kisch-ip.com>
2022-02-14 12:11:23 +0200

    Setup Ef connection to elite database and added in a query

--- KischEbilling/MainViewModel.cs
+++ KischEbilling/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KischEbilling.Models;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Data;
@@ -26,28 +27,34 @@ namespace KischEbilling
             MyCommand = new DelegateCommand(OnCommandExecuted);
             SubmitCommand = new DelegateCommand(OnSubmitCommandExecuted);
         }
-   
+        List<InvoiceRecord> data;
         public ICommand MyCommand { get; set; }
         public ICommand SubmitCommand { get; set; }
        
         private void OnCommandExecuted(object obj)
         {
-            //string fileName = @"C:\Ebiling\InvoiceListUpload.xlsx";
-            //if (!File.Exists(fileName))
-            //{
-            //    throw new FileNotFoundException(String.Format("File {0} was not found!", fileName));
-            //}
 
-            //Telerik.Windows.Documents.Spreadsheet.Model.Workbook workbook;
-            //IWorkbookFormatProvider formatProvider = new Telerik.Windows.Documents.Spreadsheet.FormatProviders.OpenXml.Xlsx.XlsxFormatProvider();
+            using (var ctx = new TE_3E_PRODEntities())
+            {
+              data = ctx.Database.SqlQuery<InvoiceRecord>("SELECT [InvMasterID],[InvDate],[InvNumber] FROM[TE_3E_PROD].[dbo].[InvMaster] where InvNumber = 'I03-0008915'; ", new SqlParameter("@case_id", 277761)).ToList();
 
-            //using (Stream input = new FileStream(fileName, FileMode.Open))
-            //{
-            //    workbook = formatProvider.Import(input);
-            //}
+            }
+                //string fileName = @"C:\Ebiling\InvoiceListUpload.xlsx";
+                //if (!File.Exists(fileName))
+                //{
+                //    throw new FileNotFoundException(String.Format("File {0} was not found!", fileName));
+                //}
+
+                //Telerik.Windows.Documents.Spreadsheet.Model.Workbook workbook;
+                //IWorkbookFormatProvider formatProvider = new Telerik.Windows.Documents.Spreadsheet.FormatProviders.OpenXml.Xlsx.XlsxFormatProvider();
+
+                //using (Stream input = new FileStream(fileName, FileMode.Open))
+                //{
+                //    workbook = formatProvider.Import(input);
+                //}
 
 
-            XlsxFormatProvider formatProvider = new XlsxFormatProvider();
+                XlsxFormatProvider formatProvider = new XlsxFormatProvider();
             Workbook workbook = formatProvider.Import(File.ReadAllBytes(@"C:\Ebiling\InvoiceListUpload.xlsx"));
 
             var worksheet = workbook.Sheets[0] as Worksheet;
@@ -92,65 +99,7 @@ namespace KischEbilling
             }
         }
 
-        private void OnSubmitCommandExecuted(object obj)
-        {
-            //LxReaderSettings lxReaderSettings = new LxReaderSettings();
-            //lxReaderSettings.ErrorHandler = MyDeserializerErrorHandler;
-
-            //var patentTransactionBody = new LxSerializer<PatentTransactionElm>();
-
-           
-            ////var data = patentTransactionBody.Deserialize(@"C:\Users\DivarR\VirtualBox VMs\XSD-ST96\Test\3\PatentTransaction-NationalPatent.xml");
-            ////var fileName = Path.Combine(
-            ////Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)
-            ////    , @"Data\PatentTransaction.xml");
-            //var data = patentTransactionBody.Deserialize(@"C:\PatentTransaction-NationalPatent.xml");
-            ////foreach(var node in data.TransactionContentBag.TransactionData.PatentApplication.BibliographicData.ApplicationIdentification.ApplicationNumber)
-            ////{
-
-            ////}
-            //try
-            //{
-            //    //foreach (var node in data.PatentTransactionBodies)
-            //    //{
-            //    //    if(node.TransactionContentBag.TransactionData.PatentApplication.BibliographicData.ApplicationIdentification.ApplicationNumber.IPOfficeCode.Value != null)
-            //    //    Console.WriteLine(node.TransactionContentBag.TransactionData.PatentApplication.BibliographicData.ApplicationIdentification.ApplicationNumber.IPOfficeCode.Value);
-            //    //}
-            //}
-            //catch(NullReferenceException e)
-            //{
-
-            //    Console.WriteLine(e.ToString());
-            //}
-
-            //LxSerializer<PatentTransactionElm> serializer = new LxSerializer<PatentTransactionElm>();
-            ////using (StringWriter sw = new StringWriter())
-            ////{
-            ////    serializer.Serialize(sw, data);
-
-            ////    Console.WriteLine(sw.ToString());
-            ////}
-
-            //LxSerializer<PatentTransactionElm> s = new LxSerializer<PatentTransactionElm>();
-
-            //using (TextReader textReader = new StreamReader(@"C:\Users\DivarR\VirtualBox VMs\XSD-ST96\Test\3\PatentTransaction-NationalPatent.xml"))
-            //{
-            //    PatentTransactionElm patentTransactionElm = s.Deserialize(textReader);
-
-
-            //    //patentTransactionElm.PatentTransactionBodiesCol.TransactionContentBag.TransactionIdentifier.Value = "batch123-1/20a";
-
-            //    patentTransactionElm.PatentTransactionBodies[0].TransactionContentBag.TransactionIdentifier.Value = "batch123 - 1 / 20a";
-
-            //    using (TextWriter textWriter = new StreamWriter(@"C:\Users\DivarR\Documents\Kisch\Tasks\46IPAS\9SourceCode\KischIpas\Kisch.Ipas\PatentTransaction-NationalPatent2.xml"))
-            //    {
-            //        serializer.Serialize(textWriter, patentTransactionElm);
-            //    }
-            //}
-
-            //Console.ReadLine();
-        }
-
+        
         private void OnCreateANewDocCommandExecuted(object obj)
         {
             //var patentTransactionBody = new LxSerializer<PatentTransactionElm>();
---
+++ KischEbilling/Models/InvoiceRecord.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telerik.Windows.Controls;
+
+namespace KischEbilling.Models
+{
+    class InvoiceRecord : ViewModelBase
+    {
+        public string InvNumber { get; set; }
+        public DateTime InvDate { get; set; }
+    }
+}
